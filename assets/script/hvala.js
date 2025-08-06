f50db16a753b3a99ceddbe2de93e50d26f1aaabe
djordje-taskovicict <161512776+djordje-taskovicict@users.noreply.github.com>
2025-08-06 23:05:25 +0200

    Update hvala.js

--- assets/script/hvala.js
+++ assets/script/hvala.js
@@ -15,7 +15,7 @@
     // Dodaj drugi paragraf sa linkom
     const para2 = document.createElement('p');
     const link = document.createElement('a');
-    link.href = '../index.html';
+    link.href = '../../index.html';
     link.textContent = 'Vrati se na početnu stranicu.';
     para2.appendChild(link);
     thankYouContainer.appendChild(para2);
