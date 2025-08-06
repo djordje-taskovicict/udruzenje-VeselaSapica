538754bfa6d2240a1255a1b5d4b917ac023e0cd0
djordje-taskovicict <161512776+djordje-taskovicict@users.noreply.github.com>
2025-08-06 23:56:10 +0200

    Update hvala.js

--- assets/script/hvala.js
+++ assets/script/hvala.js
@@ -1,25 +1,26 @@
 window.addEventListener('DOMContentLoaded', function () {
-    const thankYouContainer = document.createElement('div');
-    thankYouContainer.classList.add('thank-you-container');
+  // Kreiramo container
+  const thankYouContainer = document.createElement('div');
+  thankYouContainer.classList.add('thank-you-container');
 
-    // Dodaj naslov
-    const heading = document.createElement('h1');
-    heading.textContent = 'Hvala na vašoj prijavi!';
-    thankYouContainer.appendChild(heading);
+  // Naslov
+  const heading = document.createElement('h1');
+  heading.textContent = 'Hvala na vašoj prijavi!';
+  thankYouContainer.appendChild(heading);
 
-    // Dodaj prvi paragraf
-    const para1 = document.createElement('p');
-    para1.textContent = 'Vaš formular je uspešno poslat. Bićemo u kontaktu uskoro.';
-    thankYouContainer.appendChild(para1);
+  // Prvi paragraf
+  const para1 = document.createElement('p');
+  para1.textContent = 'Vaš formular je uspešno poslat. Bićemo u kontaktu uskoro.';
+  thankYouContainer.appendChild(para1);
 
-    // Dodaj drugi paragraf sa linkom
-    const para2 = document.createElement('p');
-    const link = document.createElement('a');
-    link.href = window.location.origin + '/udruzenje-VeselaSapica/';
-    link.textContent = 'Vrati se na početnu stranicu.';
-    para2.appendChild(link);
-    thankYouContainer.appendChild(para2);
+  // Drugi paragraf sa linkom ka pravom repozitorijumu
+  const para2 = document.createElement('p');
+  const link = document.createElement('a');
+  link.href = window.location.origin + '/udruzenje-VeselaSapica/';
+  link.textContent = 'Vrati se na početnu stranicu.';
+  para2.appendChild(link);
+  thankYouContainer.appendChild(para2);
 
-    // Dodaj dinamicki sadržaj u body
-    document.body.appendChild(thankYouContainer);
+  // Ubacimo sve u body
+  document.body.appendChild(thankYouContainer);
 });
